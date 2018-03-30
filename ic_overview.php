7f3dfe6f225c981568917917229dda30700b8b03
loadmybowl <37914786+loadmybowl@users.noreply.github.com>
2018-03-30 07:52:53 -0400

    Update ic_overview.php

--- ic_overview.php
+++ ic_overview.php
@@ -47,56 +47,6 @@
             <strong>Balance</strong> - <b><label id="pWalletBalance"></label></b>
         </div>
     </div><br/>
-
-    <div class="row">
-        <div class="col-md-1"></div>
-
-        <div class="col-md-10">
-            <section class="panel panel-primary">
-                <header class="panel-heading">
-                    <h2 class="panel-title">Send UniBits with Message</h2>
-                </header>
-                <div class="panel-body">
-                    <form>
-
-                        <div class="row form-group">
-                            <label class="col-sm-3 control-label"><strong>Send to </strong></label>
-                            <div class="col-sm-9">
-                                <input id="txtToAddrSWM" type="text" placeholder="Enter recipient's address or user name" class="form-control" value="" size=50 />
-                            </div>
-                        </div>
-                        <br/>
-
-                        <div class="row form-group">
-                            <label class="col-sm-3 control-label"><strong>Message </strong></label>
-                            <div class="col-sm-9">
-                                <textarea id="txtMessageSWM" name="txtMessageSWM" rows="5" class="form-control" placeholder="Send a message" required></textarea>
-                            </div>
-                        </div>
-                        <br/>
-
-                        <div class="row form-group">
-                            <label class="col-sm-3 control-label"><strong>Yourcoins </strong></label>
-                            <div class="col-sm-9">
-                                <input id="txtUnitsSWM" type="number" step="any" placeholder="" class="form-control" value="1" width=30 />
-                            </div>
-                        </div>
-                        <br/>
-
-                        <div class="row form-group">
-                            <div class="col-sm-9">
-                                <input type="reset" class="mb-xs mt-xs mr-xs btn btn-primary" onclick="sendMetadataToAddress('txtMyAddress', 'txtToAddrSWM', 'txtMessageSWM', 'txtUnitsSWM', this, 'outputSWM');" value="Send">
-                            </div>
-                        </div><br/><br/>
-
-                        <div id="outputSWM" class="appear-animation bounceIn appear-animation-visible">
-                        </div>
-
-                    </form>
-                </div>
-            </section>
-        </div>
-
         <div class="col-md-1"></div>
 
     </div>
