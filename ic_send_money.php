c6499abc4795b3fdefbb7039e2bc0b58bbd6ee9f
loadmybowl <37914786+loadmybowl@users.noreply.github.com>
2018-03-30 07:59:47 -0400

    Update ic_send_money.php

--- ic_send_money.php
+++ ic_send_money.php
@@ -20,34 +20,12 @@
 
 <div class="container theme-showcase" role="main">
 
-    <h2>Web-Wallet</h2>
+    <h2>Send UniBit</h2>
     <br>
-
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-4">
-            <div class="row">
-                <label><strong>Your Wallet Address</strong></label>
-            </div>
-            <div class="row">
-                <input id="txtMyAddress" type="text" class="form-control" size=45 value="<?php echo $_SESSION['address'] ?>" readonly="readonly" />
-            </div>
-        </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-2">
-            <div class="row">
-                <label><strong>Your User name</strong></label>
-            </div>
-            <div class="row">
-                <input id="txtMyUserName" type="text" class="form-control" size=45 value="<?php echo $_SESSION['user_name'] ?>" readonly="readonly" />
-            </div>
-        </div>
         <div class="col-md-1"></div>
         <div class="col-md-2">
             <strong>Balance</strong> - <b><label id="pWalletBalance"></label></b>
         </div>
-    </div><br/>
-
     <div class="row">
         <div class="col-md-1"></div>
 
